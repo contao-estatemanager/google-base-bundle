755d06c1b4c007e167e3be83b6e7c13452c28c26
doishub <daniele@oveleon.de>
2020-12-14 11:36:43 +0100

    Remove AddonManager

--- src/Resources/contao/dca/tl_real_estate_config.php
+++ src/Resources/contao/dca/tl_real_estate_config.php
@@ -8,21 +8,20 @@
  * @license   https://www.contao-estatemanager.com/lizenzbedingungen.html
  */
 
-if(ContaoEstateManager\GoogleMaps\AddonManager::valid()) {
-    // Add field
-    // ToDo: Rename field in googlemaps bundle
-    $GLOBALS['TL_DCA']['tl_real_estate_config']['fields']['googleApiToken'] = array
-    (
-        'label'                   => &$GLOBALS['TL_LANG']['tl_real_estate_config']['googleApiToken'],
-        'inputType'               => 'text',
-        'eval'                    => array('tl_class'=>'w50')
-    );
+// Add field
+// ToDo: Rename field in googlemaps bundle
+$GLOBALS['TL_DCA']['tl_real_estate_config']['fields']['googleApiToken'] = array
+(
+    'label'                   => &$GLOBALS['TL_LANG']['tl_real_estate_config']['googleApiToken'],
+    'inputType'               => 'text',
+    'eval'                    => array('tl_class'=>'w50')
+);
 
-    // ToDo: Rename legend in googlemaps bundle
-    // Extend default palette
-    Contao\CoreBundle\DataContainer\PaletteManipulator::create()
-        ->addLegend('google_legend', 'filter_config_legend', Contao\CoreBundle\DataContainer\PaletteManipulator::POSITION_AFTER)
-        ->addField(array('googleApiToken'), 'google_legend', Contao\CoreBundle\DataContainer\PaletteManipulator::POSITION_APPEND)
-        ->applyToPalette('default', 'tl_real_estate_config')
-    ;
-}
+// ToDo: Rename legend in googlemaps bundle
+// Extend default palette
+Contao\CoreBundle\DataContainer\PaletteManipulator::create()
+    ->addLegend('google_legend', 'filter_config_legend', Contao\CoreBundle\DataContainer\PaletteManipulator::POSITION_AFTER)
+    ->addField(array('googleApiToken'), 'google_legend', Contao\CoreBundle\DataContainer\PaletteManipulator::POSITION_APPEND)
+    ->applyToPalette('default', 'tl_real_estate_config')
+;
+
