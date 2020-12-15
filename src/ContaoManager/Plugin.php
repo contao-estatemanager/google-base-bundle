6077ce246e38da54f7f40afd834e3aa6c72ad120
eki89 <fabian@oveleon.de>
2020-12-15 09:06:44 +0100

    Composer cleanup

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -10,7 +10,7 @@
 
 declare(strict_types=1);
 
-namespace ContaoEstateManager\GoogleBaseBundle\ContaoManager;
+namespace ContaoEstateManager\GoogleServices\ContaoManager;
 
 use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
